91682b185d26c18430720b642d7d2f3a19c04bda
Guilhermessena <guilhermedvsena@gmail.com>
2024-02-21 20:48:11 -0300

    tela pre checkin, e de finalização

---
+++ lab_clinicas_adm/lib/src/pages/pre_checkin/pre_checkin_controller.dart
@@ -0,0 +1,23 @@
+import 'package:lab_clinicas_adm/src/model/patient_information_form_model.dart';
+import 'package:lab_clinicas_adm/src/service/call_next_patient/call_next_patient_service.dart';
+import 'package:lab_clinicas_core/lab_clinicas_core.dart';
+import 'package:signals_flutter/signals_flutter.dart';
+
+class PreCheckinController with MessageStateMixin {
+  PreCheckinController({required CallNextPatientService callNextPatientService})
+      : _callNextPatientService = callNextPatientService;
+  final CallNextPatientService _callNextPatientService;
+  final informationForm = signal<PatientInformationFormModel?>(null);
+
+  Future<void> next()async {
+    final result = await _callNextPatientService.execute();
+    switch(result){
+      case Left():
+      showError('Erro ao chamar paciente');
+      case Right(value: final form?):
+      informationForm.value = form;
+      case Right():
+      showInfo('Nenhum paciente aguardando');
+    }
+  }
+}
